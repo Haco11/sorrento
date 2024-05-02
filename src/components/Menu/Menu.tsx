4d5251150de6c5369ec0489d3e457d1ebb91a2c9
Haci11 <46707887+Haci11@users.noreply.github.com>
2024-05-02 23:36:38 +0200

    Refactor Menu component to display categories and meals dynamically, and add client-side rendering support

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,36 +1,57 @@
-import React from "react";
-import { categories } from "@/data"; // path to your data.ts file
-
+"use client";
+import React, { useState } from "react";
+import { categories } from "@/data";
+import { Button } from "../ui/button";
 type Props = {};
 
 const Menu = (props: Props) => {
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
   return (
-    <div className="space-y-8 container mx-auto px-4">
-      {categories.map((category) => (
-        <div key={category.id} className="space-y-4">
-          <h1 className="text-2xl font-bold text-gray-800">{category.title}</h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {category.meals.map((meal) => (
-              <div
-                key={meal.id}
-                className="border border-gray-200 p-4 rounded-lg space-y-2">
-                <h2 className="text-xl font-semibold text-gray-700">
-                  {meal.name}
-                </h2>
-                <p className="text-lg font-bold text-gray-800">
-                  {meal.price} kr
-                </p>
-                <ul className="list-none pl-0 gap-2 flex flex-wrap text-sm text-gray-600">
-                  {meal.ingredients.map((ingredient, index) => (
-                    <li key={index}>{ingredient},</li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-          </div>
+    <>
+      <div className="space-y-8 container mx-auto px-4 mt-4">
+        <div className="flex gap-3 flex-wrap items-center justify-center p-4 rounded-md">
+          {categories.map((category) => (
+            <Button
+              key={category.id}
+              variant="outline"
+              onClick={() => setSelectedCategory(category.id)}
+              className="uppercase">
+              {category.title}
+            </Button>
+          ))}
+          <Button variant="outline" onClick={() => setSelectedCategory(null)}>
+            VISA ALLA
+          </Button>
         </div>
-      ))}
-    </div>
+
+        {categories
+          .filter(
+            (category) => !selectedCategory || selectedCategory === category.id
+          )
+          .map((category) => (
+            <div key={category.id} className="space-y-4">
+              <h1 className="text-2xl font-bold uppercase ">
+                {category.title}
+              </h1>
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                {category.meals.map((meal) => (
+                  <div
+                    key={meal.id}
+                    className="border border-gray-200 p-4 rounded-lg space-y-2">
+                    <h2 className="text-xl font-semibold">{meal.name}</h2>
+                    <p className="text-lg font-bold ">{meal.price} kr</p>
+                    <ul className="list-none pl-0 space-x-2 flex flex-wrap text-sm ">
+                      {meal.ingredients.map((ingredient, index) => (
+                        <li key={index}>{ingredient}</li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
   );
 };
 
